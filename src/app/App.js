c5fb4cb531be385a79012b0934d5e8219c764a14
ghigbie <gehigbie@gmail.com>
2018-01-26 20:59:32 -0700

    Add div for new element

--- src/app/App.js
+++ src/app/App.js
@@ -27,9 +27,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-main">
+        </div>
       </div>
     );
   }
