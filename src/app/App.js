8c49d36f62954649a9b12907dbe59eed27ca32b9
ghigbie <gehigbie@gmail.com>
2018-01-26 22:03:26 -0700

    Add product info

--- src/app/App.js
+++ src/app/App.js
@@ -10,30 +10,38 @@ class App extends Component {
   constructor(props){
     super(props);
     this.loadData = this.loadData.bind(this);
+    this.productList = this.productList.bind(this);
 
     this.state = {
       products: []
     };
-    productList = () => {
-      const list = this.state.products.map((product) => {
-         <div className="col-sm-4"
-              key={product._id}>
-              <Product title={product.title}
-                       price={product.price}
-                       imgUrl={product.imgUrl}/>
-         </div>
-      });
-    }
+
     this.loadData();
   }
 
   loadData = () => {
-    http.getProducts().then(products => {
-      console.log(products);
+    var self = this;
+    http.getProducts().then(data => {
+      console.log(data);
+      self.setState({products: data});
     }, err => {
 
     });
   }
+
+
+  productList = () => {
+    const list = this.state.products.map((product) =>
+      <div className="col-sm-4"
+           key={product._id}>
+        <Product title={product.title}
+                 price={product.price}
+                 imgUrl={product.imgUrl} />
+      </div>);
+
+    return list;
+  }
+
 
   render() {
     return (
@@ -44,18 +52,7 @@ class App extends Component {
         </header>
         <div className="App-main container">
           <div className="row flex-justify">
-            <Product className="col-sm-4"
-                     price="123.23"
-                     title="Cool Floating Cat"
-                     imgUrl="http://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg" />
-            <Product className="col-sm-4"
-                     price="123.23"
-                     title="Cool Floating Cat"
-                     imgUrl="http://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg" />
-            <Product className="col-sm-4"
-                     price="123.23"
-                     title="Cool Floating Cat"
-                     imgUrl="http://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg" />
+            {this.productList()}
           </div>
         </div>
       </div>
--- src/product/product.js
+++ src/product/product.js
@@ -2,9 +2,6 @@ import React, {Component} from 'react';
 import './product.css';
 
 class Product extends Component{
-    constructor(props){
-        super(props);
-    }
 
     render(){
         return(
@@ -15,7 +12,7 @@ class Product extends Component{
                 <div className="card-block">
                     <h4 className="card-title">{this.props.title}</h4>
                     <p className="card-text">Price: ${this.props.title}</p>
-                    <a href="#" className="btn btn-primary">Add to Wishlist</a>
+                    <a href="www.google.com" className="btn btn-primary">Add to Wishlist</a>
                 </div>
             </div>
         );
